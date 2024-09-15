1580cae0a8d6a3e00b910082188968df8776a18c
JTR781888 <mohamedsama1212@gmail.com>
2024-09-15 12:27:58 -0400

    My first hour making the website
    I made the main template of the page, added a navigational bar. An about section was made and completed and the main details including the name, small description were added.

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '../Styles/modern-normalise.css';
+import '../Styles/style.css';
+import '../Styles/components/header.css'
+import '../Styles/components/hero.css'
+import '../Styles/components/about.css'
+import '../Styles/components/featured.css'
+import '../Styles/utils.css';
